941f7107a7f6493e623b104625a8e95ca42a8997
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-02 16:32:07 +0200

    flex layouts

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Box, Button, Flex, HStack, Heading, Spacer, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Flex as="nav" p="10px" alignItems="center" >
+     <Heading as="h1" >Dojo Tasks</Heading>
+
+     <Spacer/>
+     <HStack spacing="10px">
+
+     <Box bg="gray.200" p="10px">M</Box>
+
+     <Text>paccy@gmail.com</Text>
+     <Button colorScheme='purple'>Logout</Button>
+     </HStack>
+    </Flex>
+      
+  )
+}
+
+export default Navbar
