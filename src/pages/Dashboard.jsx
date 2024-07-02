a124ddeea2f58f12654cbe073d6f733b25a32efd
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-02 15:00:10 +0200

    components in chakra ui

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Container as="section">
+      <Heading my="30px" p="10px">Chackra UI</Heading>
+      <Text ml="30px" color="blue.300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Ut voluptatum excepturi ad placeat amet eum iusto accusamus. 
+        Earum animi aliquam sint quod amet veritatis fugit? Dolore suscipit neque voluptates placeat?</Text>
+
+        <Box my="30px" p="20px" bg="yellow">
+          <Text color="black">The text is the box</Text>
+        </Box>
+    </Container>
   )
 }
